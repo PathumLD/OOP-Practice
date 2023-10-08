7a37b06a7db7f128e30cf8f936aa13a9a20195ab
PathumLD <2020is031@stu.ucsc.cmb.ac.lk>
2023-10-08 19:36:11 +0530

    Change

--- src/Exeptional_Handling/ExceptionalHandling.java
+++ src/Exeptional_Handling/ExceptionalHandling.java
@@ -4,24 +4,21 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionalHandling {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int a,b,c;
-        try{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // sc can be anything
+        int a, b, c;
+        try {
             System.out.print("Enter first no :");
             a = sc.nextInt();
             System.out.print("Enter second no :");
             b = sc.nextInt();
-            c = a/b;
-            System.out.println("Answer is " +c);
-        }
-        catch(ArithmeticException e){
+            c = a / b;
+            System.out.println("Answer is " + c);
+        } catch (ArithmeticException e) { // e can be anything
             System.out.println("Can't devide by zero");
-        }
-        catch(InputMismatchException e){
+        } catch (InputMismatchException e) {
             System.out.println("Enter an integer number");
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
